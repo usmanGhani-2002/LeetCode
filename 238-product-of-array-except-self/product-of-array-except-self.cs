ef3f9740ae380ce1dd083c4214af8f65e5cd8457
usmanGhani-2002 <musmanghani6658@gmail.com>
2025-10-01 07:38:52 -0700

    Time: 1 ms (100.00%) | Memory: 66.6 MB (84.52%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int n = nums.Length;
+        int[] answer = new int[n];
+
+        // Step 1: Prefix products
+        answer[0] = 1;
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        // Step 2: Multiply with suffix products
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return answer;
+    }
+}
